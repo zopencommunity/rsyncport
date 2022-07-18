693d12a1e80e44af86af7610f192e56a6defa67d
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-18 08:47:04 -0400

    Add description to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux') 
 {
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'rsyncport'), string(name: 'DESCRIPTION', 'rsyncport' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'rsyncport'), string(name: 'DESCRIPTION', 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
         }
 }
