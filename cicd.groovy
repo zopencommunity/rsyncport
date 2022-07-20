ae4a606910200c16f4ba4e5dd3beca3458c5b320
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-20 10:00:54 -0400

    Add full repo url

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'rsyncport'), string(name: 'PORT_DESCRIPTION', value: 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/rsyncport.git'), string(name: 'PORT_DESCRIPTION', value: 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
         }
 }
