6e336f491bf04dccd2176516f8214a2ed7c5a12e
Igor Todorovski <itodorov@ca.ibm.com>
2022-07-19 21:21:37 -0400

    Remove polling of utils.git

--- cicd.groovy
+++ cicd.groovy
@@ -10,6 +10,6 @@ node('linux')
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'rsyncport'), string(name: 'DESCRIPTION', 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'REPO', value: 'rsyncport'), string(name: 'DESCRIPTION', value: 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
         }
 }
