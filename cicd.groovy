3186598845c245bd4be6f158e5a719090ca98cda
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:39 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/rsyncport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/rsyncport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/rsyncport.git'), string(name: 'PORT_DESCRIPTION', value: 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/rsyncport.git'), string(name: 'PORT_DESCRIPTION', value: 'rsync is a utility for efficiently transferring and synchronizing files between a computer and a storage drive and across networked computers by comparing the modification times and sizes of files.' )]
         }
 }
