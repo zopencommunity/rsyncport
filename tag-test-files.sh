b1308a019f0e4d2aa5c1929c947ab50a5fcccc8c
D Harithamma <harithamma.d@ibm.com>
2026-08-28 07:23:22 -0400

    Add script to tag test files as UTF-8 on z/OS
    After cloning on z/OS, the test files are tagged as ISO8859-1 but
    contain UTF-8 characters (Ñ, ñ, etc.), causing Python to fail.
    
    Run this script after clone to properly tag the test files:
      ./tag-test-files.sh
    
    This tags the three z/OS CCSID test files as UTF-8 so Python can
    correctly read them with '# -*- coding: utf-8 -*-' declaration.
    
    Usage on z/OS after clone:
      git clone https://github.com/zopencommunity/rsyncport.git
      cd rsyncport
      git checkout bump-rsync-version-3.5.0
      ./tag-test-files.sh
      cd rsync-3.5.0
      make check

---
+++ tag-test-files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Tag z/OS CCSID test files as UTF-8 after clone/checkout
+# These files contain UTF-8 test data and must be tagged correctly
+
+cd "$(dirname "$0")/rsync-3.5.0/testsuite"
+
+echo "Tagging z/OS CCSID test files as UTF-8..."
+
+chtag -tc UTF-8 zos-ccsid-conversion_test.py
+chtag -tc UTF-8 zos-ccsid-performance_test.py  
+chtag -tc UTF-8 zos-ccsid-regression_test.py
+
+echo
+echo "Verifying tags:"
+ls -T zos-ccsid-*.py
+
+echo
+echo "✓ Done! Test files are now tagged as UTF-8"
